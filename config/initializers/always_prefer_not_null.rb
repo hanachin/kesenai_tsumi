e642433967ca4aa39869aead638e155e5e41afa6
Seiei Miyagi <hanachin@gmail.com>
2020-06-07 17:01:24 +0900

    Always prefer null: false for add_column

--- config/initializers/always_prefer_not_null.rb
+++ config/initializers/always_prefer_not_null.rb
@@ -1,5 +1,14 @@
+
 ActiveRecord::ConnectionAdapters::TableDefinition.prepend(Module.new {
   def column(name, type, index: nil, **options)
     super(name, type, index: index, **options.merge(null: false))
+  end
+})
+
+# NOTE: PostgreSQLAdapter is not loaded before connecion resolved
+require "active_record/connection_adapters/postgresql_adapter"
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(Module.new {
+  def add_column(table_name, column_name, type, **options)
+    super(table_name, column_name, type, **options.merge(null: false))
   end
 })
