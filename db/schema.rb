c71196017a9317697d0424a6c0b686b9e426977a
Seiei Miyagi <hanachin@gmail.com>
2020-06-08 02:09:25 +0900

    dc run --rm app rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_164139) do
+ActiveRecord::Schema.define(version: 2020_06_07_170618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2020_06_07_164139) do
     t.index ["post_id"], name: "index_post_cancellations_on_post_id"
   end
 
+  create_table "post_post_cancellations", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "post_cancellation_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_cancellation_id"], name: "index_post_post_cancellations_on_post_cancellation_id"
+    t.index ["post_id", "post_cancellation_id"], name: "index_post_post_cancellation_unique"
+    t.index ["post_id"], name: "index_post_post_cancellations_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.bigint "board_id", null: false
     t.string "poster", null: false
@@ -38,5 +48,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_164139) do
   end
 
   add_foreign_key "post_cancellations", "posts"
+  add_foreign_key "post_post_cancellations", "post_cancellations"
+  add_foreign_key "post_post_cancellations", "posts"
   add_foreign_key "posts", "boards"
 end
