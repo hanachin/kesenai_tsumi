9b59818e1d31396a5b457f3f70c5041c86bbdecf
Seiei Miyagi <hanachin@gmail.com>
2020-06-07 15:48:15 +0900

    Really destroy the post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_board
+  before_action :set_post, only: :destroy
 
   # POST /boards/:board_id/posts
   # POST /boards/:board_id/posts.json
@@ -17,13 +18,27 @@ class PostsController < ApplicationController
     end
   end
 
+  # DELETE /boards/:board_id/posts/1
+  # DELETE /boards/:board_id/posts/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to @board, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
+
+    def post_params
+      params.require(:post).permit(:poster, :body)
+    end
 
     def set_board
       @board = Board.find(params[:board_id])
     end
 
-    def post_params
-      params.require(:post).permit(:poster, :body)
+    def set_post
+      @post = @board.posts.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'boards#index'
   resources :boards do
-    resources :posts, only: :create
+    resources :posts, only: %I[create destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
